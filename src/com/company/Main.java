8379d6c63455e763ca38d101cf7f456728432eb7
AnaKostova <anakostova13@gmail.com>
2023-05-28 21:11:45 +0200

    coomitr

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) { System.out.println("Hello World");
     sayHello("Lili");
 	// write your code here
+        System.out.println("Deana");
     }
 
 public static void sayHello(String name) {System.out.println("Hello" +name); }
